54f7b2b40fe52f7022dd3f8da6a00569f1bddcba
deiner restrepo <metallica082011@hotmail.com>
2015-03-14 18:27:44 -0500

    plantillas configuras

--- SistemaAcademico/settings.py
+++ SistemaAcademico/settings.py
@@ -82,5 +82,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'estaticos/estaticos'),)
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'estaticos/plantillas'),)
