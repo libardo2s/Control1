760c4bfacc38507edd0bc545a7dda7f2e4e63560
libardo2s <libardoii@hotmail.com>
2015-03-14 13:14:07 -0500

    14/02/2015 01:13 Pm

--- SistemaAcademico/settings.py
+++ SistemaAcademico/settings.py
@@ -83,4 +83,4 @@ USE_TZ = True
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'AppSistemaAcademico/estaticos/plantillas'),)
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'estaticos/plantillas'),)
